5d94a44892de83ce45085988f8999dfc95d9ecf2
ElsabethBaye <115937111+ElsabethBaye@users.noreply.github.com>
2026-04-14 10:58:08 +0300

    Add files via upload

---
+++ nmap scanner.py
@@ -0,0 +1,22 @@
+import socket
+
+target = input("Enter target IP: ")
+start_port = int(input("Start port: "))
+end_port = int(input("End port: "))
+
+print(f"\nScanning {target} from port {start_port} to {end_port}...\n")
+
+for port in range(start_port, end_port + 1):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1)
+
+    result = s.connect_ex((target, port))
+
+    if result == 0:
+        print(f"[+] Port {port} is OPEN")
+    else:
+        print(f"[-] Port {port} is CLOSED")
+
+    s.close()
+
+print("\nScan completed.")
